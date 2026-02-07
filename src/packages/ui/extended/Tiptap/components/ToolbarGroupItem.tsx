003c90db4c34b2271b8840c4fed0606e106eb113
guanweisong <307761682@qq.com>
2026-02-07 12:52:58 +0800

    feat: use DropdownMenu instead Tooltip in Tiptap

--- src/packages/ui/extended/Tiptap/components/ToolbarGroupItem.tsx
+++ src/packages/ui/extended/Tiptap/components/ToolbarGroupItem.tsx
@@ -1,9 +1,20 @@
 import { Editor } from "@tiptap/core";
 import { ToolbarGroup } from "../config/toolbarItems";
-import { ToolbarButton } from "./ToolbarButton";
-import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import { cn } from "@/packages/ui/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/packages/ui/components/dropdown-menu";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/packages/ui/components/tooltip";
+import { Button } from "@/packages/ui/components/button";
 
 interface ToolbarGroupItemProps {
   editor: Editor;
@@ -11,7 +22,6 @@ interface ToolbarGroupItemProps {
 }
 
 export function ToolbarGroupItem({ editor, group }: ToolbarGroupItemProps) {
-  const [open, setOpen] = useState(false);
   const { items, icon: groupIcon, label: groupLabel } = group;
 
   const activeItem = items.find((item) => item.isActive?.(editor));
@@ -20,47 +30,57 @@ export function ToolbarGroupItem({ editor, group }: ToolbarGroupItemProps) {
   const triggerLabel = activeItem ? activeItem.label : groupLabel;
 
   return (
-    <ToolbarButton
-      label={triggerLabel}
-      active={!!activeItem}
-      open={open}
-      onOpenChange={setOpen}
-      className={cn(
-        "p-0 flex items-center w-auto h-9 hover:bg-gray-100 transition-colors",
-        activeItem && "bg-purple-50 hover:bg-purple-100",
-      )}
-      icon={
-        <div className="flex items-center justify-center px-1 h-full rounded-md hover:bg-black/5 transition-colors">
-          {triggerIcon}
-          <ChevronDown className="h-3 w-3 opacity-50" />
-        </div>
-      }
-    >
-      <div className="flex flex-col">
+    <DropdownMenu>
+      <TooltipProvider delayDuration={200}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <DropdownMenuTrigger asChild>
+              <Button
+                type="button"
+                size="icon"
+                variant="ghost"
+                className={cn(
+                  "p-0 flex items-center w-auto h-9 hover:bg-gray-100 transition-colors",
+                  activeItem &&
+                    "bg-purple-50 hover:bg-purple-100 !text-purple-600",
+                )}
+              >
+                <div className="flex items-center justify-center px-1 h-full rounded-md hover:bg-black/5 transition-colors">
+                  {triggerIcon}
+                  <ChevronDown className={"h-3 w-3 opacity-50"} />
+                </div>
+              </Button>
+            </DropdownMenuTrigger>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">{triggerLabel}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <DropdownMenuContent align="start" className="min-w-auto">
         {items.map((item) => {
           if (item.render) {
             return <div key={item.label}>{item.render(editor)}</div>;
           }
           return (
-            <div
+            <DropdownMenuItem
               key={item.label}
-              className={cn(
-                "flex gap-1 leading-8 cursor-pointer items-center rounded-md px-2 hover:bg-gray-100",
-                {
-                  "text-purple-600": item.isActive?.(editor),
-                },
-              )}
               onClick={() => {
                 item.onClick?.(editor);
-                setOpen(false);
               }}
             >
-              {item.icon}
-              {item.label}
-            </div>
+              <span
+                className={cn(
+                  "flex gap-2 cursor-pointer items-center",
+                  item.isActive?.(editor) && "text-purple-600",
+                )}
+              >
+                {item.icon}
+                {item.label}
+              </span>
+            </DropdownMenuItem>
           );
         })}
-      </div>
-    </ToolbarButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
